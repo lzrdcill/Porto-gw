8596b6ef3629a4e0f7311b00cd51096c60322123
lzrdcill <lazuardimahdihakim13@gmail.com>
2025-06-29 09:31:23 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log("Website Jasa Digital aktif!");
+
+// Efek kursor berubah saat hover pada elemen interaktif
 document.querySelectorAll(".card-hover").forEach(card => {
   card.style.cursor = "pointer";
 });
